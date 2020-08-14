9de2836af6eb4c4ff394f4f026dee6b95b3a9bdc
andrewhan1 <dno_arta@mail.ru>
2020-08-14 12:59:19 +0300

    Updates

---
+++ precache-manifest.ab53915939e9b12dec244023b03ba5c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7b28e3d8987b8448e7ed090fd4a259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a763626c81cd1dbcfa6f",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9e235733a8dac02ba4cf",
+    "url": "/static/css/main.dc8fa0fc.chunk.css"
+  },
+  {
+    "revision": "a763626c81cd1dbcfa6f",
+    "url": "/static/js/2.44f12904.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "/static/js/2.44f12904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e235733a8dac02ba4cf",
+    "url": "/static/js/main.6ca00d07.chunk.js"
+  },
+  {
+    "revision": "92a3ac8c3dc9682eada8",
+    "url": "/static/js/runtime-main.71649b55.js"
+  },
+  {
+    "revision": "0b307f2890fc772f6013e8b08aa80e7c",
+    "url": "/static/media/kar1.0b307f28.jpg"
+  },
+  {
+    "revision": "29da21a4ce9834d2859e2fe8d3a66b86",
+    "url": "/static/media/kar2.29da21a4.jpg"
+  }
+]);
